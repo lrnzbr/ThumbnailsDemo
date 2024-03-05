b8afb636cb6e43af2346a3e34dba7d20e1bc57b6
Lorenzo Brown <Lorenzo.Brown@dowjones.com>
2024-03-05 13:20:43 -0600

    Initial Commit

---
+++ ThumbnailsDemo/ThumbnailsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThumbnailsDemoApp.swift
+//  ThumbnailsDemo
+//
+//  Created by Lorenzo Brown on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ThumbnailsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
